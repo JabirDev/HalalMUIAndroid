5e856a887955934b343526209d61049b77729079
Taufan Fatahillah <taufan.fhm8@gmail.com>
2020-01-15 09:46:17 +0700

    update example

--- app/src/main/java/com/jabirdeveloper/halalmuiandroid/MainActivity.java
+++ app/src/main/java/com/jabirdeveloper/halalmuiandroid/MainActivity.java
@@ -6,10 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-//import com.jabirdeveloper.halalmui.HalalListener;
-//import com.jabirdeveloper.halalmui.HalalMui;
-//import com.jabirdeveloper.halalmui.HalalData;
-//import com.squareup.picasso.Picasso;
+import com.jabirdeveloper.halalmui.HalalListener;
+import com.jabirdeveloper.halalmui.HalalMui;
+import com.jabirdeveloper.halalmui.HalalData;
 
 import java.util.List;
 
@@ -23,29 +22,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final TextView tv = findViewById(R.id.textView);
         String n = "indomie";
-//        HalalMui.byNamaProduk(n, new HalalListener() {
-//            @Override
-//            public void ketikaSukses(List<HalalData> halalData) {
-//                StringBuilder sb = new StringBuilder();
-//                for (int i = 0; i < halalData.size(); i++) {
-//                    sb.append(halalData.get(i).getNamaProduk());
-//                    sb.append("\n");
-//                }
-//                tv.setText(sb.toString());
-//                Log.d(TAG, sb.toString());
-//            }
-//
-//            @Override
-//            public void ketikaTidakAdaData(String s) {
-//                tv.setText(s);
-//                Log.e(TAG, s);
-//            }
-//
-//            @Override
-//                public void ketikaGagal(Throwable t) {
-//                tv.setText(t.getMessage());
-//                Log.e(TAG, "ketikaGagal: ", t);
-//            }
-//        });
+        HalalMui.byNamaProduk(n, new HalalListener() {
+            @Override
+            public void ketikaSukses(List<HalalData> halalData) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < halalData.size(); i++) {
+                    sb.append(halalData.get(i).getNamaProduk());
+                    sb.append("\n");
+                }
+                tv.setText(sb.toString());
+                Log.d(TAG, sb.toString());
+            }
+
+            @Override
+            public void ketikaTidakAdaData(String s) {
+                tv.setText(s);
+                Log.e(TAG, s);
+            }
+
+            @Override
+                public void ketikaGagal(Throwable t) {
+                tv.setText(t.getMessage());
+                Log.e(TAG, "ketikaGagal: ", t);
+            }
+        });
     }
 }
